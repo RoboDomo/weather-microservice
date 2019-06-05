725d75afe376ac70e773c8df0ebf675bfac1e97d
Michael Schwartz <mykesx@gmail.com>
2019-06-04 18:47:29 -0700

    ha -> robodomo

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   mqtt: {
     // host where mqtt server resides
     // You can add an entry to this server's /etc/hosts to point
-    // the name 'ha' at the mqtt server, or set the MQTT_HOST env
+    // the name 'robodomo' at the mqtt server, or set the MQTT_HOST env
     // variable with your mqtt connect string.
-    host:  process.env.MQTT_HOST || 'mqtt://ha',
+    host: process.env.MQTT_HOST || "mqtt://robodomo",
     // This is the base of the topic used to publish/subscribe.
     // For example, autelis/# (on the client) to listen to all updates
     // or autelis/jets to listen for events about the Spa jets.
@@ -12,11 +12,13 @@ module.exports = {
     //      topic: autelist/jets
     //      message: on
     //
-    topic: process.env.topic || 'weather',
+    topic: process.env.topic || "weather"
   },
   weather: {
-    locations: process.env.WEATHER_LOCATIONS ? process.env.WEATHER_LOCATIONS.split(',') : [
-      '92211',    // Palm Desert CA, it's HOT!!!
-    ]
-  },
-}
+    locations: process.env.WEATHER_LOCATIONS
+      ? process.env.WEATHER_LOCATIONS.split(",")
+      : [
+          "92211" // Palm Desert CA, it's HOT!!!
+        ]
+  }
+};
